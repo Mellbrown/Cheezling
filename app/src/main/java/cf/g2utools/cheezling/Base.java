c872a1fa0621710d4f07bdb81b87f4d8a1eb1626
Mellbrown <mlyg2588@hotmail.com>
2017-12-08 14:14:16 +0900

    안드로이드 프로젝트 생성 - 프로젝트 명 : .치즐링.. - 안드로이드 최소 지원 버전 : 마시멜로

---
+++ app/src/main/java/cf/g2utools/cheezling/Base.java
@@ -0,0 +1,13 @@
+package cf.g2utools.cheezling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Base extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_base);
+    }
+}
