4616f2404c09374d6b91f905ee0b58519b63e5e2
Mellbrown <mlyg2588@hotmail.com>
2017-12-09 22:29:40 +0900

    테마 및 하이브리드 테스트 - 컬러 정의 - 테마 정의 - 웹 테스트

---
+++ app/src/main/java/cf/g2utools/cheezling/lab/Mold_Test.java
@@ -0,0 +1,15 @@
+package cf.g2utools.cheezling.lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cf.g2utools.cheezling.R;
+
+public class Mold_Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_a_mold);
+    }
+}
